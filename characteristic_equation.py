0d819852860a1b997ec22787f8a4d36b06ea078f
user <ermolin.andrei2017@yandex.ru>
2023-09-19 21:39:49 +0300

    add parsing

---
+++ characteristic_equation.py
@@ -0,0 +1,7 @@
+class CharEquation:
+    equation: str
+
+    def __init__(self, equation: str):
+        self.equation = equation
+
+
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from differce_equation import *
 
 if __name__ == '__main__':
+    s = input()
+    alphabet = input().split()
+    DiffEquation(s, alphabet).parse()
     print("")
